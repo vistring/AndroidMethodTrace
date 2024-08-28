db429566040763b0cbbf974d428c0d05f8c75876
Chen Jiang <jiangc@vistring.com>
2024-08-28 14:33:30 +0800

    build: Add task to publish trace plugin and its dependencies to mavenLocal
    This commit introduces a new Gradle task `publishToMavenLocal` that publishes the
     trace plugin and its dependencies to the local Maven repository. This simplifies the process of testing and integrating the trace plugin with other projects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,10 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+// 通过 includeBuild 引入的 trace-plugin 作为独立的模块，默认并不会包含进来，需要手动添加
+tasks.register("publishToMavenLocal") {
+    dependsOn(gradle.includedBuild("trace-plugin").task(":publishToMavenLocal"))
+    dependsOn(subprojects.filter { it.tasks.findByName("publishToMavenLocal") != null }
+        .map { it.tasks.named("publishToMavenLocal") })
+}
